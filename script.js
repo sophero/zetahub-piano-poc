3a42d1549e15c79e1f168470875bd3543efd34a3
sophero <sophia.simons.di@gmail.com>
2019-02-12 17:19:04 -0500

    Add email signup piano custom event handler

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Load ZetaHub P13N.js library
-
   var SITEID='souremedia-dev'; // DEV
   // var SITEID='sourcemedia-prod'; // PROD
   (function(b, t, r, a, i, n) {
@@ -20,5 +19,31 @@ document.addEventListener("DOMContentLoaded", function() {
   // DEV
      'https://cdn.boomtrain.com/p13n/'+SITEID+'/p13n.js'
   );
-});  
 
+  // Set up Piano callbacks.
+  // Uses code from https://docs.piano.io/how-to-create-a-newsletter-signup-with-templates/#customngform
+  tp = window.tp || [];
+  tp.push(["addHandler", "customEvent", function(event) {
+    switch (event.eventName) {
+      case 'email-signup':
+        console.log('received email customcheckout event');
+        var email = '';
+        var params;
+
+        // We are parsing the params object sent from the template to find out which iframe triggered it
+        params = JSON.parse(event.params.params);
+        console.log('params:', params)
+        if ((typeof event.params.email != 'undefined') && (event.params.email.length > 0)) {
+            email = event.params.email;
+        }
+        // pass the relevant data to ZetaHub. Invoke p13n library's track function.
+        bt(
+          'track',
+          'signed_up',
+          { email },
+          { onSuccess: function() { console.log(`ZETA: User ${email} successfully signed up`); }});
+        break;
+    }
+  }]);
+
+});
